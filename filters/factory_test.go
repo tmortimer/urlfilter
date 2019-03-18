4cca70a4485d58c46786de5629fcb78b97fbe3d2
Tom Mortimer <tmortime@gmail.com>
2019-03-17 19:35:04 -0700

    Last few filter factory tests

--- filters/factory_test.go
+++ filters/factory_test.go
@@ -2,27 +2,49 @@ package filters
 
 import (
 	"github.com/tmortimer/urlfilter/config"
+	"github.com/tmortimer/urlfilter/connectors"
 	"testing"
 )
 
 //TOM test the TypeOf these filters
 
-func TestCreateFilterSuccess(t *testing.T) {
+func TestCreateFakeFilterSuccess(t *testing.T) {
 	config := config.NewConfig()
-	_, err := CreateFilter("fake", config)
+	filter, err := CreateFilter("fake", config)
+
 	if err != nil {
-		t.Errorf("Creating a Fake filter generated an error: %s", err)
+		t.Fatalf("Creating a Fake filter generated an error: %s", err)
 	}
 
-	_, err = CreateFilter("redis", config)
+	_, ok := filter.(*Fake)
+	if !ok {
+		t.Fatalf("A filter other than Fake was created.")
+	}
+}
+
+func TestCreateRedisFilterSuccess(t *testing.T) {
+	config := config.NewConfig()
+	filter, err := CreateFilter("redis", config)
+
 	if err != nil {
-		t.Errorf("Creating a Redis filter generated an error: %s", err)
+		t.Fatalf("Creating a Redis filter generated an error: %s", err)
+	}
+
+	db, ok := filter.(*DB)
+	if !ok {
+		t.Fatalf("A filter other than DB was created.")
+	}
+
+	_, ok = db.conn.(*connectors.Redis)
+	if !ok {
+		t.Fatalf("A handler other than Redis was created.")
 	}
 }
 
 func TestCreateFilterFailure(t *testing.T) {
 	config := config.NewConfig()
 	_, err := CreateFilter("wzzl", config)
+
 	if err == nil {
 		t.Errorf("Trying to create a filter type that does not exist failed to generate an error.")
 	}
@@ -31,19 +53,27 @@ func TestCreateFilterFailure(t *testing.T) {
 func TestFilterFactorySuccess(t *testing.T) {
 	config := config.NewConfig()
 	config.Filters = []string{"redis", "fake"}
-	_, err := FilterFactory(config)
+	filter, err := FilterFactory(config)
+
 	if err != nil {
 		t.Errorf("Creating a Fake filter generated an error: %s", err)
 	}
 
-	//TOM need to do some reflection shenanigans here too
-	/*if filter.next == nil {
+	db, ok := filter.(*DB)
+	if !ok {
+		t.Fatalf("A filter other than DB was created.")
+	}
+
+	if db.next == nil {
 		t.Errorf("The filters were not chained together properly.")
 	}
 
-	if filter.next.next != nil {
-		t.Errorf("There are extra filters in the chain.")
-	}*/
+	_, ok = db.next.(*Fake)
+	if !ok {
+		t.Fatalf("A filter other than Fake was created.")
+	}
+
+	// No Secondary filter in fake, so G2G.
 }
 
 func TestFilterFactoryFailure(t *testing.T) {
